0721d7173545dfd7fa22ab40e832b01ac4afd6d4
== <=>
2023-02-19 23:57:46 +0200

    test4

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class Home extends StatelessWidget {
           child: Image(
           // image:NetworkImage('https://www.w3schools.com/css/img_forest.jpg'),
        //     image:AssetImage('assets/2.png'),
-            image:AssetImage('assets/1.png'),
+          //  image:AssetImage('assets/1.png'),
           )),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
